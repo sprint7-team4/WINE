511c77f26bd748ddf72f40b7e2c01cb7649f711a
Dangchan Kim <kdc5847@naver.com>
2024-08-01 16:10:06 +0900

    :sparkles: Feat: Header 조건부렌더링을 위한 함수생성 및 조건부렌더링

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Header from "@/components//common/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const showHeader = pageProps.showHeader !== false; // Header컴포넌트 조건부렌더링을 위한 변수
+
+  return (
+    <>
+      {showHeader && <Header />}
+      <Component {...pageProps} />
+    </>
+  );
 }
