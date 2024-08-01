1fe717b89f9e6afc2ab3394ba52f9a4e543204e7
IllllIIllIIl <mymail@site.com>
2024-08-01 23:57:50 +0900

    ✨ Feat: 상단 이달의 추천 와인 목록 컴포넌트 제작
    이미지는 현재 없어서 주석으로 대체했음

--- src/lib/wineApi.ts
+++ src/lib/wineApi.ts
@@ -1,14 +1,28 @@
-import axios from './axios';
-import { GetWinesParams, GetWinesResponse } from '../types/wineTypes';
+import axios from "./axios";
+import { GetWinesParams, GetWinesResponse, Wine } from "../types/wineTypes";
 
 export const getWines = async (
   params: GetWinesParams
 ): Promise<GetWinesResponse> => {
   try {
-    const response = await axios.get<GetWinesResponse>('wines', { params });
-    return response.data;
+    const res = await axios.get<GetWinesResponse>("wines", { params });
+    return res.data;
   } catch (error) {
-    console.error('Error fetching wines', error);
+    console.error("Error fetching wines", error);
+    throw error;
+  }
+};
+
+export const getRecommendedWines = async (
+  params: GetWinesParams
+): Promise<Wine[]> => {
+  try {
+    const res = await axios.get<Wine[]>("wines/recommended", {
+      params,
+    });
+    return res.data;
+  } catch (error) {
+    console.log("Error fetching recommended Wines", error);
     throw error;
   }
 };
