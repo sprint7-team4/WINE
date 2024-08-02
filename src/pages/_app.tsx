dab1ee11283a3f39f8ef3307ae27cd264f743652
서미영 <seomiyoung3998@gmail.com>
2024-08-02 15:36:22 +0900

    💄 Style: _app에 페이지별 영역 제외 수정

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,25 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Header from "@/components//common/Header";
+import Header from "@/components/common/Header";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const showHeader = pageProps.showHeader !== false; // Header컴포넌트 조건부렌더링을 위한 변수
+  const showHeader = pageProps.showHeader !== false; // Header 컴포넌트의 조건부 렌더링
 
   const router = useRouter();
   const isAuthPage =
     router.pathname === "/login" || router.pathname === "/signup";
+  const isExcludeMainArea = isAuthPage || router.pathname === "/";
+
+  const mainClassName = `
+  ${!isAuthPage ? "max-w-1140 m-[0_auto] px-16 md:px-20 lg:px-0" : "p-0"}
+  ${isExcludeMainArea ? "mt-0" : "mt-[66px] md:mt-[94px]"}
+`.trim();
 
   return (
     <>
       {showHeader && <Header />}
-      <main
-        className={
-          isAuthPage
-            ? "mt-0"
-            : "max-w-1140 m-[0_auto] mt-[66px] md:mt-[94px] px-16 md:px-20 lg:px-0"
-        }
-      >
+      <main className={mainClassName}>
         <Component {...pageProps} />
       </main>
     </>
