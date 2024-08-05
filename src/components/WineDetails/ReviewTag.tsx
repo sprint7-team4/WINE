a30c390ba9659f2a57fc58fcafa3a1fc4a4fb7ba
myong39 <166721604+myong39@users.noreply.github.com>
2024-08-05 21:06:30 +0900

    Delete src/components/WineDetails directory

--- src/components/WineDetails/ReviewTag.tsx
+++
@@ -1,9 +0,0 @@
-const ReviewTag = ({ tag }: { tag: string }) => {
-  return (
-    <h3 className="flex flex-center rounded-100 p-[6px_10px] md:p-[8px_15px] font-medium-16 text-grayscale-800 border border-grayscale-300 border-solid">
-      {tag}
-    </h3>
-  );
-};
-
-export default ReviewTag;
