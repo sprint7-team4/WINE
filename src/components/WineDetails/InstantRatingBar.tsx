c23231423b25bca85210a665e62d2918e83803ed
서미영 <seomiyoung3998@gmail.com>
2024-08-05 16:00:05 +0900

    💄 Style: 별점 바 width 수정

--- src/components/WineDetails/InstantRatingBar.tsx
+++ src/components/WineDetails/InstantRatingBar.tsx
@@ -29,7 +29,7 @@ const InstantRatingBar = ({
   return (
     <div className="flex w-full md:max-w-241 relative">
       <div
-        className="w-full md:max-w-241 bg-grayscale-100 h-6 rounded-50 absolute left-0 -translate-y-1/2"
+        className="w-full md:w-241 bg-grayscale-100 h-6 rounded-50 absolute left-0 -translate-y-1/2"
         ref={containerRef}
       ></div>
       <div
