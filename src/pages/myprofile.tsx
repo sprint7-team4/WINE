4731c5e82ad5e5b82a839e946b493e361feefc0c
Dangchan Kim <kdc5847@naver.com>
2024-08-12 19:35:14 +0900

    테그 수정

--- src/pages/myprofile.tsx
+++ src/pages/myprofile.tsx
@@ -167,7 +167,7 @@ export default function Myprofile() {
               </div>
             </div>
             <div className="w-full">
-              <form className="w-full flex flex-col gap-8 md:flex-row md:items-end md:justify-between md:gap-24 lg:flex-col lg:gap-8">
+              <div className="w-full flex flex-col gap-8 md:flex-row md:items-end md:justify-between md:gap-24 lg:flex-col lg:gap-8">
                 <div
                   className="flex flex-col gap-8  
                               md:gap-10  "
@@ -191,7 +191,7 @@ export default function Myprofile() {
                     onClick={handleUpdateNickname}
                   />
                 </div>
-              </form>
+              </div>
             </div>
           </div>
         </div>
