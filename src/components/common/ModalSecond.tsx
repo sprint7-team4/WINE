c8464ebb086477e212de96e4feda2b78504dc412
Dangchan Kim <kdc5847@naver.com>
2024-08-12 23:22:18 +0900

    :sparkles: Feat: 좀 더 편리한 Modal 컴포넌트 구현

---
+++ src/components/common/ModalSecond.tsx
@@ -0,0 +1,39 @@
+import { ReactNode, useEffect, useRef } from "react";
+import useModalSecondStore from "@/store/ModalSecondStore";
+
+interface Props {
+  children: ReactNode;
+  className?: string;
+  id: string;
+}
+
+export default function Dialog({ children, id, ...rest }: Props) {
+  const { modals, openSecondModal, closeSecondModal } = useModalSecondStore();
+  const isOpen = modals[id] || false;
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  useEffect(() => {
+    if (dialogRef.current) {
+      if (isOpen) {
+        dialogRef.current.showModal();
+      } else {
+        dialogRef.current.close();
+      }
+    }
+  }, [isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      openSecondModal(id);
+    }
+    return () => {
+      closeSecondModal(id);
+    };
+  }, [id]);
+
+  return (
+    <dialog ref={dialogRef} {...rest}>
+      {children}
+    </dialog>
+  );
+}
