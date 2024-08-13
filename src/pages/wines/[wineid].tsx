73b2b9b5b5e5d6627236f2cf2f300e1b97b8678e
myong39 <166721604+myong39@users.noreply.github.com>
2024-08-13 19:26:27 +0900

    Update [wineid].tsx

--- src/pages/wines/[wineid].tsx
+++ src/pages/wines/[wineid].tsx
@@ -106,6 +106,7 @@ const WineDetailPage = ({ wine }: { wine: WineReview }) => {
             hasMore={hasMore}
             loader={<h4>Loading...</h4>}
             endMessage={<p>마지막 리뷰입니다.</p>}
+            style={{ overflow: "hidden" }}
           >
             <div className="flex flex-col lg:flex-row gap-20">
               <div className="flex flex-col gap-16">
